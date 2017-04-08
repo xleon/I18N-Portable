deb29008858b1db516e4b395cf3c221cb7f7be3b
Diego Ponce de Leon <malandro@gmail.com>
2017-04-08 03:17:01 +0200

    Windows sample

---
+++ Sample.Classic.Core/BaseViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using I18NPortable;
+using PropertyChanged;
+using Sample.Classic.Core.Annotations;
+
+namespace Sample.Classic.Core
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+        public II18N Strings => I18N.Current;
+        public string LoadedLanguage => I18N.Current.Language.DisplayName;
+        public string AvailableLanguages => string.Join(", ", I18N.Current.Languages.Select(x => x.DisplayName));
+        public string Dog => Animals.Dog.Translate();
+        public string EnumValues => string.Join(", ", I18N.Current.TranslateEnumToList<Animals>());
+        public string GreetingValue => "Greeting.Value".Translate("Windows");
+
+        public IEnumerable<PortableLanguage> LanguagesToSelect 
+            => I18N.Current.Languages
+                .Where(x => x.Locale != I18N.Current.Locale);
+
+        public void LoadLocale(string locale)
+        {
+            I18N.Current.Locale = locale;
+
+            OnPropertyChanged(nameof(LoadedLanguage));
+            OnPropertyChanged(nameof(AvailableLanguages));
+            OnPropertyChanged(nameof(Dog));
+            OnPropertyChanged(nameof(EnumValues));
+            OnPropertyChanged(nameof(GreetingValue));
+        }
+    }
+}
--- Sample.Classic.Uwp/MainPage.xaml.cs
+++ Sample.Classic.Uwp/MainPage.xaml.cs
@@ -1,30 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+using Sample.Classic.Core;
 
 namespace Sample.Classic.Uwp
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
+        public BaseViewModel ViewModel => (BaseViewModel) DataContext;
+
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+            DataContext = new BaseViewModel();
+        }
+
+        private void ChangeLanguageButtonClick(object sender, RoutedEventArgs e)
+        {
+            var menu = new MenuFlyout();
+
+            foreach (var language in ViewModel.LanguagesToSelect)
+            {
+                var item = new MenuFlyoutItem {Text = language.DisplayName, Tag = language.Locale};
+                item.Click += ItemOnClick;
+                menu.Items.Add(item);
+            }
+
+            menu.ShowAt((FrameworkElement)sender);
+        }
+
+        private void ItemOnClick(object sender, RoutedEventArgs routedEventArgs)
+        {
+            var menuItem = (MenuFlyoutItem) sender;
+            var locale = (string) menuItem.Tag;
+
+            ViewModel.LoadLocale(locale);
         }
     }
 }
