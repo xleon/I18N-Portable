075e914916ef6d4d1b2eadddadf2bd84d94828c9
Diego Ponce de Leon <malandro@gmail.com>
2017-04-21 04:41:36 +0200

    Add some tests

--- I18NPortable.UnitTests/EmbeddedLocaleProviderTests.cs
+++ I18NPortable.UnitTests/EmbeddedLocaleProviderTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using I18NPortable.Providers;
 using NUnit.Framework;
 
@@ -10,13 +12,17 @@ namespace I18NPortable.UnitTests
         [Test]
         public void ResourceFolder_CanBe_Set()
         {
-            throw new NotImplementedException();
+            var provider = new EmbeddedLocaleProvider(GetType().Assembly, "OtherLocales").Init();
+            var locales = provider.GetAvailableLocales().ToList();
+
+            Assert.Contains("es", locales);
+            Assert.Contains("en", locales);
         }
 
         [Test]
-        public void SameLocale_Cannot_BeAddedTwice()
+        public void Locales_CannotBe_Duplicated()
         {
-            var provider = new EmbeddedLocaleProvider(GetType().Assembly, "BadLocales");
+            var provider = new EmbeddedLocaleProvider(GetType().Assembly, "DuplicatedLocales");
 
             Assert.Throws<Exception>(() => provider.Init());
         }
@@ -24,25 +30,30 @@ namespace I18NPortable.UnitTests
         [Test]
         public void OnlyKnownFileExtensions_ShouldBe_Read()
         {
-            throw new NotImplementedException();
+            var provider = new EmbeddedLocaleProvider(GetType().Assembly).Init();
+            var locales = provider.GetAvailableLocales().ToList();
+
+            Assert.IsFalse(locales.Contains("es-CO"));
         }
 
         [Test]
         public void KnownFileExtensions_Cannot_BeEmpty()
         {
-            throw new NotImplementedException();
-        }
+            var provider = new EmbeddedLocaleProvider(GetType().Assembly, knownFileExtensions:new List<string>());
 
-        [Test]
-        public void TxtExtension_WillBeUsed_When_KnownFileExtensions_IsNull()
-        {
-            throw new NotImplementedException();
+            Assert.Throws<Exception>(() => provider.Init());
         }
 
         [Test]
         public void AnyFileExtensions_CanBe_Loaded()
         {
-            throw new NotImplementedException();
+            var knowExtensions = new[] {".weird1", ".weird2", ".weird3"};
+            var provider = new EmbeddedLocaleProvider(GetType().Assembly, "OtherLocales", knowExtensions).Init();
+            var locales = provider.GetAvailableLocales().ToList();
+
+            Assert.Contains("fr", locales);
+            Assert.Contains("ru", locales);
+            Assert.Contains("pt", locales);
         }
     }
 }
