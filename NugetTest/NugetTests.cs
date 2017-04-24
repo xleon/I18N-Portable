3f3b8e33e48e255b1f3d78e8e6457562cda70a52
Diego Ponce de Leon <malandro@gmail.com>
2017-04-24 20:01:24 +0200

    New nuget for I18NPortable.JsonReader

---
+++ NugetTest/NugetTests.cs
@@ -0,0 +1,36 @@
+﻿using I18NPortable;
+using I18NPortable.JsonReader;
+using NUnit.Framework;
+
+namespace NugetTest
+{
+    [TestFixture]
+    public class NugetTests
+    {
+        [Test]
+        public void JsonKvpReader_ShouldRead_JsonLocales()
+        {
+            I18N.Current = new I18N()
+                .SetResourcesFolder("JsonKvpLocales")
+                .AddLocaleReader(new JsonKvpReader(), ".json")
+                .Init(GetType().Assembly);
+
+            I18N.Current.Locale = "es";
+
+            Assert.AreEqual("uno", "one".Translate());
+        }
+
+        [Test]
+        public void JsonListReader_ShouldRead_JsonLocales()
+        {
+            I18N.Current = new I18N()
+                .SetResourcesFolder("JsonListLocales")
+                .AddLocaleReader(new JsonListReader(), ".json")
+                .Init(GetType().Assembly);
+
+            I18N.Current.Locale = "es";
+
+            Assert.AreEqual("uno", "one".Translate());
+        }
+    }
+}
