eb338dce497eeb4af93c0f3b98dfcc67397f9cdd
enisn <iletisim@enisnecipoglu.com>
2021-02-09 17:03:01 +0300

    Add Xamarin XAML Extensions

---
+++ I18NPortable.Xamarin/Xaml/Extensions/TranslateExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace I18NPortable.Xamarin.Xaml.Extensions
+{
+    [ContentProperty("Key")]
+    public class TranslateExtension : IMarkupExtension<string>
+    {
+        public string Key { get; set; }
+
+        public object[] Args { get; set; }
+
+        public string ProvideValue(IServiceProvider serviceProvider)
+        {
+            return I18N.Current.Translate(Key, Args ?? new object[0]);
+        }
+
+        object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider)
+        {
+            return ProvideValue(serviceProvider);
+        }
+    }
+}
---
+++ I18NPortable.Xamarin/Xaml/Extensions/TranslateOrNullExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace I18NPortable.Xamarin.Xaml.Extensions
+{
+    [ContentProperty("Key")]
+    public class TranslateOrNullExtension : IMarkupExtension<string>
+    {
+        public string Key { get; set; }
+
+        public object[] Args { get; set; }
+
+
+        public string ProvideValue(IServiceProvider serviceProvider)
+        {
+            return I18N.Current.TranslateOrNull(Key, Args ?? new object[0]);
+        }
+
+        object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider)
+        {
+            return ProvideValue(serviceProvider);
+        }
+    }
+}
